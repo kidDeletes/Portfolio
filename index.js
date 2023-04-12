dff7572f0731d08da64a5cda4471f21dfd63e854
kidDeletes <84948166+kidDeletes@users.noreply.github.com>
2023-04-12 23:03:48 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+let toggle = document.querySelector('.toggle');
+let topbar = document.querySelector('.topbar');
+let navigation = document.querySelector('.navigation');
+let  main = document.querySelector('.main');
+let themeSwitch = document.querySelector('.themeSwitch');
+let body = document.querySelector('body');
+
+toggle.onclick = function ()
+{
+	toggle.classList.toggle('active');
+	topbar.classList.toggle('active');
+	navigation.classList.toggle('active');
+	main.classList.toggle('active');
+}
+
+//themeSwitch 
+themeSwitch.onclick = function()
+{
+	body.classList.toggle('dark');
+}
+
+// fixing menu active class for mobile devices
+function toggleMenu()
+{
+	let navigation = document.querySelector('.navigation');
+	let  main = document.querySelector('.main');
+	navigation.classList.remove('active');
+	main.classList.remove('active');
+}
+
